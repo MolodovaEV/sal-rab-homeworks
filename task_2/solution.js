9545229d43c1889deb66b4dd5a9bc301f3dcc9fa
Molodova E <mollenka89@mail.ru>
2023-03-31 07:22:36 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -4,7 +4,14 @@ function calcShipping(sum, min, shipping) {
     let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
-
+    let shippingSum;
+    if  (productsSum == 0) {
+        shippingSum = 0;
+    }   else if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }   else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
     // создайте переменную shippingSum
 
     // если productsSum равно 0,
@@ -27,7 +34,12 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-
+    let discountSum;
+    if (productsSum >= discountMinSum) {
+        discountSum = productsSum * discountPart / 100;
+    }   else {
+        discountSum = 0;
+    }
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -44,7 +56,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
-
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
     // создайте переменную totalSum
 
     // присвойте totalSum значение productsSum
@@ -53,7 +67,10 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    shippingSum == 0;
+    freeShipping = (!shippingSum);
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
